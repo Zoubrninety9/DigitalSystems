d9fc69998dd65c5360535aea31d55f18ad481940
Zoubrninety9 <91428796+Zoubrninety9@users.noreply.github.com>
2023-02-01 09:38:21 +0000

    1

--- tasks/Task212-Generate/invN.sv
+++ tasks/Task212-Generate/invN.sv
@@ -1,5 +1,5 @@
 // Example of generate for to replicate hardware
-module invN #(parameter N=8) (output logic[N-1:0] Y, input logic[N-1:0] X);
+module invN #(parameter N=2) (output logic[N-1:0] Y, input logic[N-1:0] X);
 
 //An integer that will never be represented in the final synthesis
 genvar i;
@@ -7,7 +7,11 @@ genvar i;
 generate 
  for (i=0; i<N; i=i+1) 
  begin : i_loop1
-	not ui (Y[i], X[i]);
+ if (i == 0 | i ==2)
+	assign Y[i] = ~X[i];
+	else
+	assign Y[i] = X[i];
+	
  end
 endgenerate
 
