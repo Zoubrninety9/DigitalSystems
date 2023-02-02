a3e2ec2c399cced06ebd9aba2d0bdcca63a85e04
Zoubrninety9 <91428796+Zoubrninety9@users.noreply.github.com>
2023-02-02 14:39:42 +0000

    1

--- tasks/Task218-Addition/adder_N.sv
+++ tasks/Task218-Addition/adder_N.sv
@@ -8,13 +8,13 @@ output logic Cout;
 input logic [N-1:0] A, B;
 input logic Cin;
 
-output logic [7:4]Sum2; 
-output logic Cout2;
-input logic [7:4] A2, B2;
-input logic Cin2;
+//output logic [7:4]Sum2; 
+//output logic Cout2;
+//input logic [7:4] A2, B2;
+//input logic Cin2;
 
 always_comb
 	{Cout, Sum} = A + B + Cin;
-	assign Cout = Cin2;
-	{Cout2, Sum2} = A2 + B2 + Cin2;
+	// Cout = Cin2;
+	//{Cout2, Sum2} = A2 + B2 + Cin2;
 endmodule
